0c6bd2d454a8243638d49c51e9635640d2d9d4a3
fulvaz <fulvaz@foxmail.com>
2017-01-17 21:35:25 +0800

    be

---
+++ src/components/common/ImgEditor.vue
@@ -0,0 +1,50 @@
+<template lang="html">
+  <div class="editor-container" v-show="show">
+    <layout>
+      <div class="img-editor">
+        <img :src="img">
+      </div>
+    </layout>
+  </div>
+</template>
+
+<script>
+let that
+import layout from './layout/PopupEditorLayout'
+export default {
+  data () {
+    return {
+      show: false
+    }
+  },
+  created () {
+    that = this
+  },
+  props: {
+    img: String
+  },
+  components: {
+    layout: layout
+  },
+  beforeDestroyed () {
+    that = ''
+
+  },
+  open () {
+    that.show = true
+  },
+  close () {
+    that.show = false
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .img-editor {
+    width: 100vw;
+    height: 100vw;
+  }
+  img {
+    max-width: 100%;
+  }
+</style>
---
+++ src/components/common/layouts/PopupEditorLayout.vue
@@ -0,0 +1,178 @@
+<template lang="html">
+  <div v-show="show" class="post-editor-container">
+    <header>
+      <span class="title">圈子发言</span>
+      <button class="send-btn btn" @click="handleSend">发布</button>
+      <button class="close-btn btn" @click="close">关闭</button>
+    </header>
+    <section class="editor">
+      <slot></slot>
+    </section>
+    <!-- <section class="functions">
+    </section> -->
+  </div>
+</template>
+
+<script>
+import * as api from '../../api/index.js'
+import * as utils from '../../utils/utils.js'
+import SinglePicker from '../common/SinglePicker'
+let that
+export default {
+  components: {
+    'fz-picker': SinglePicker
+  },
+  props: {
+    category: {
+      type: Object,
+      default: {}
+    }
+  },
+  data () {
+    return {
+      show: false,
+      title: '',
+      content: '',
+      type: ''
+    }
+  },
+  computed: {
+    slotVal () {
+      return utils.obj2arr(this.category)
+    }
+  },
+  created () {
+    that = this // 给全局函数使用
+  },
+  watch: {
+    category () {
+      if (this.category.length === 0) return
+      this.type = utils.obj2arr(this.category)[0]
+    }
+  },
+  methods: {
+    handleSend (e) {
+      let data = {
+        title: this.title,
+        content: this.content,
+        uid: this.$store.state.MeState.uid,
+        circleId: parseInt(this.$route.params.id),
+        date: (new Date()).toString(),
+        type: parseInt(utils.value2Key(this.category, this.type))
+      }
+      this.$validator.validateAll().then(success => {
+        if (!success) return
+        this.openIndicator()
+        api.newPost(data).then(response => {
+          this.handleSuccess('NEW_POST_SUCCESS')
+          this.$emit('input', data)
+          // 清空输入框
+          this.title = ''
+          this.content = ''
+          this.close()
+        }).catch(response => {
+          console.error(response)
+          this.handleFail('NEW_POST_FAIL')
+        })
+      })
+    },
+    close () {
+      this.show = false
+    }
+  },
+  open () {
+    that.show = true
+  },
+  close () {
+    that.show = false
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+  @import '../../assets/index.scss';
+  .post-editor-container {
+    z-index: 1000;
+    position: fixed;
+    box-sizing: border-box;
+    background-color: white;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+  }
+
+  header {
+    border-bottom: 1px solid #F2F2F2;
+
+    @include clearfix()
+    .title {
+      z-index: -1;
+      position: absolute;
+      display: block;
+      padding: 14px 0;
+      width: 100%;
+      height: 17px;
+      line-height: 17px;
+      font-size: 17px;
+      text-align: center;
+    }
+
+    .send-btn {
+      float: left;
+    }
+
+    .close-btn {
+      float: right;
+    }
+
+    .btn {
+      padding: 14px 18px;
+      color: #42bd56;
+      line-height: 17px;
+      font-size: 17px;
+      border: none;
+      background-color: transparent;
+    }
+  }
+
+  .editor {
+    padding: 0px 18px;
+    font-size: 17px;
+    .title {
+      width: 100%;
+      line-height: 45px;
+      border-bottom: 1px solid #F2F2F2;
+      &::-webkit-input-placeholder {
+        font-size: 17px;
+        font-weight: 500;
+        color: #C1C1C1;
+      }
+    }
+    .content {
+        padding: 15px 0;
+        width: 100%;
+        height: 200px;
+        font-size: 17px;
+        &::-webkit-input-placeholder {
+          font-size: 17px;
+          color: #C1C1C1;
+        }
+    }
+  }
+
+  .functions {
+      height: 45px;
+      border-bottom: 1px solid #F2F2F2;
+      border-top: 1px solid #F2F2F2;
+
+      .type {
+        border: $list-border;
+      }
+  }
+
+  .errMsg {
+    @include errMsg;
+  }
+</style>
