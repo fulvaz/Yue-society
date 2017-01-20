578093b87e109e6ed9b5269b721763e6d89c329f
fulvaz <fulvaz@foxmail.com>
2017-01-20 16:24:30 +0800

    d

---
+++ src/components/me/AddressPickerForSpouse.vue
@@ -0,0 +1,140 @@
+<template lang="html">
+<div class="container">
+  <mt-popup
+          ref="popup"
+          class="popup"
+          position="bottom"
+          v-model="visible"
+          popup-transition="popup-fade">
+    <mt-picker ref="picker" :slots="picker" @change="onChange"></mt-picker>
+  </mt-popup>
+
+  <fz-input :label="label" :placeholder="placeholder" :value="valDisplay" @click.native="handleClick" readonly  :hasError="hasError" :errMsg="errMsg"></fz-input>
+</div>
+</template>
+
+<script>
+import { Picker, Field, Popup } from 'mint-ui'
+import InputField from './InputField'
+import * as utils from '../../utils/utils.js'
+import config from '../../config/setting.js'
+
+const address = {
+  '不限': ['不限'],
+  '北京': ['北京'],
+  '广东': ['广州', '深圳', '珠海', '汕头', '韶关', '佛山', '江门', '湛江', '茂名', '肇庆', '惠州', '梅州', '汕尾', '河源', '阳江', '清远', '东莞', '中山', '潮州', '揭阳', '云浮'],
+  '上海': ['上海'],
+  '天津': ['天津'],
+  '重庆': ['重庆'],
+  '辽宁': ['沈阳', '大连', '鞍山', '抚顺', '本溪', '丹东', '锦州', '营口', '阜新', '辽阳', '盘锦', '铁岭', '朝阳', '葫芦岛'],
+  '江苏': ['南京', '苏州', '无锡', '常州', '镇江', '南通', '泰州', '扬州', '盐城', '连云港', '徐州', '淮安', '宿迁'],
+  '湖北': ['武汉', '黄石', '十堰', '荆州', '宜昌', '襄樊', '鄂州', '荆门', '孝感', '黄冈', '咸宁', '随州', '恩施土家族苗族自治州', '仙桃', '天门', '潜江', '神农架林区'],
+  '四川': ['成都', '自贡', '攀枝花', '泸州', '德阳', '绵阳', '广元', '遂宁', '内江', '乐山', '南充', '眉山', '宜宾', '广安', '达州', '雅安', '巴中', '资阳', '阿坝藏族羌族自治州', '甘孜藏族自治州', '凉山彝族自治州'],
+  '陕西': ['西安', '铜川', '宝鸡', '咸阳', '渭南', '延安', '汉中', '榆林', '安康', '商洛'],
+  '河北': ['石家庄', '唐山', '秦皇岛', '邯郸', '邢台', '保定', '张家口', '承德', '沧州', '廊坊', '衡水'],
+  '山西': ['太原', '大同', '阳泉', '长治', '晋城', '朔州', '晋中', '运城', '忻州', '临汾', '吕梁'],
+  '河南': ['郑州', '开封', '洛阳', '平顶山', '安阳', '鹤壁', '新乡', '焦作', '濮阳', '许昌', '漯河', '三门峡', '南阳', '商丘', '信阳', '周口', '驻马店'],
+  '吉林': ['长春', '吉林', '四平', '辽源', '通化', '白山', '松原', '白城', '延边朝鲜族自治州'],
+  '黑龙江': ['哈尔滨', '齐齐哈尔', '鹤岗', '双鸭山', '鸡西', '大庆', '伊春', '牡丹江', '佳木斯', '七台河', '黑河', '绥化', '大兴安岭地区'],
+  '内蒙古': ['呼和浩特', '包头', '乌海', '赤峰', '通辽', '鄂尔多斯', '呼伦贝尔', '巴彦淖尔', '乌兰察布', '锡林郭勒盟', '兴安盟', '阿拉善盟'],
+  '山东': ['济南', '青岛', '淄博', '枣庄', '东营', '烟台', '潍坊', '济宁', '泰安', '威海', '日照', '莱芜', '临沂', '德州', '聊城', '滨州', '菏泽'],
+  '安徽': ['合肥', '芜湖', '蚌埠', '淮南', '马鞍山', '淮北', '铜陵', '安庆', '黄山', '滁州', '阜阳', '宿州', '巢湖', '六安', '亳州', '池州', '宣城'],
+  '浙江': ['杭州', '宁波', '温州', '嘉兴', '湖州', '绍兴', '金华', '衢州', '舟山', '台州', '丽水'],
+  '福建': ['福州', '厦门', '莆田', '三明', '泉州', '漳州', '南平', '龙岩', '宁德'],
+  '湖南': ['长沙', '株洲', '湘潭', '衡阳', '邵阳', '岳阳', '常德', '张家界', '益阳', '郴州', '永州', '怀化', '娄底', '湘西土家族苗族自治州'],
+  '广西': ['南宁', '柳州', '桂林', '梧州', '北海', '防城港', '钦州', '贵港', '玉林', '百色', '贺州', '河池', '来宾', '崇左'],
+  '江西': ['南昌', '景德镇', '萍乡', '九江', '新余', '鹰潭', '赣州', '吉安', '宜春', '抚州', '上饶'],
+  '贵州': ['贵阳', '六盘水', '遵义', '安顺', '铜仁地区', '毕节地区', '黔西南布依族苗族自治州', '黔东南苗族侗族自治州', '黔南布依族苗族自治州'],
+  '云南': ['昆明', '曲靖', '玉溪', '保山', '昭通', '丽江', '普洱', '临沧', '德宏傣族景颇族自治州', '怒江傈僳族自治州', '迪庆藏族自治州', '大理白族自治州', '楚雄彝族自治州', '红河哈尼族彝族自治州', '文山壮族苗族自治州', '西双版纳傣族自治州'],
+  '西藏': ['拉萨', '那曲地区', '昌都地区', '林芝地区', '山南地区', '日喀则地区', '阿里地区'],
+  '海南': ['海口', '三亚', '五指山', '琼海', '儋州', '文昌', '万宁', '东方', '澄迈县', '定安县', '屯昌县', '临高县', '白沙黎族自治县', '昌江黎族自治县', '乐东黎族自治县', '陵水黎族自治县', '保亭黎族苗族自治县', '琼中黎族苗族自治县'],
+  '甘肃': ['兰州', '嘉峪关', '金昌', '白银', '天水', '武威', '酒泉', '张掖', '庆阳', '平凉', '定西', '陇南', '临夏回族自治州', '甘南藏族自治州'],
+  '宁夏': ['银川', '石嘴山', '吴忠', '固原', '中卫'],
+  '青海': ['西宁', '海东地区', '海北藏族自治州', '海南藏族自治州', '黄南藏族自治州', '果洛藏族自治州', '玉树藏族自治州', '海西蒙古族藏族自治州'],
+  '新疆': ['乌鲁木齐', '克拉玛依', '吐鲁番地区', '哈密地区', '和田地区', '阿克苏地区', '喀什地区', '克孜勒苏柯尔克孜自治州', '巴音郭楞蒙古自治州', '昌吉回族自治州', '博尔塔拉蒙古自治州', '石河子', '阿拉尔', '图木舒克', '五家渠', '伊犁哈萨克自治州'],
+  '香港': ['香港'],
+  '澳门': ['澳门'],
+  '台湾': ['台北市', '高雄市', '台北县', '桃园县', '新竹县', '苗栗县', '台中县', '彰化县', '南投县', '云林县', '嘉义县', '台南县', '高雄县', '屏东县', '宜兰县', '花莲县', '台东县', '澎湖县', '基隆市', '新竹市', '台中市', '嘉义市', '台南市']
+}
+
+export default {
+  props: {
+    label: String,
+    placeholder: String,
+    value: String,
+    errMsg: '',
+    hasError: false
+  },
+  components: {
+    'mt-field': Field,
+    'mt-picker': Picker,
+    'mt-popup': Popup,
+    'fz-input': InputField
+  },
+  computed: {
+    province () {
+      if (this.value && this.value.length > 0) return this.value.split('-')[0]
+      else return ''
+    },
+    city () {
+      if (this.value && this.value.length > 0) return this.value.split('-')[1]
+      else return ''
+    },
+    valDisplay () {
+      // 处理'不限'选项的显示问题, 默认显示是'不限 - 不限'
+      // 当远程获取的值为空字符串时, 也返回不限
+      if (this.province === '不限' || this.city === '不限' || this.value === '' || !this.value) return '不限'
+      return this.province + ' - ' + this.city
+    }
+  },
+  data () {
+    return {
+      picker: [],
+      visible: false,
+      slotVal: address
+    }
+  },
+  created () {
+    // 设定pick默认指定值，还有一处在handleClick
+    this.picker = utils.pickerHelper([Object.keys(address), address['不限']])
+  },
+  methods: {
+    onChange (picker, values) {
+      // !!!!!注意!!!!!!  下面的代码充满了魔法, 需要改picker的话, 还不如自己去找一个组件重写
+
+      // 组件的一个小bug 以后再说
+      if (this.province === undefined || this.city === undefined) return // 防止刚开始空值出乱子, setSlotValue会触发onChange
+      // 初始化时, valuse[0]是undefined
+      if (values[0] === undefined) values[0] = this.province
+
+      picker.setSlotValues(1, this.slotVal[values[0]])
+      if (config.dev) console.log(values)
+      // 处理'不限'选项的传递给父组件的数据, 默认是'不限 - 不限', 应该只传递'不限'
+      // updated: 不要做蠢事, 父组件乖乖处理 '不限-不限', 否则将要处理更多问题
+      // 为这种问题写魔法代码不值得
+      this.$emit('input', values[0] + '-' + values[1])
+    },
+    handleClick () {
+      // 设置初值
+      if (this.province.length && this.picker[0].values.indexOf(this.province) !== -1) this.$refs.picker.setSlotValue(0, this.province + '')
+      else this.$refs.picker.setSlotValue(0, '不限')
+      if (this.city.length && this.picker[2].values.indexOf(this.city) !== -1) this.$refs.picker.setSlotValue(1, this.city + '')
+      else this.$refs.picker.setSlotValue(1, '不限')
+
+      this.visible = true
+    },
+    open () {
+      this.visible = true
+    }
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+.container {
+}
+.popup {
+  width: 100%;
+}
+</style>
